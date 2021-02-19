4ffe098c9d55736e78cfe391f5a7d85c55352253
AndoHopar <64177983+AndoHopar@users.noreply.github.com>
2021-02-18 20:47:11 -0800

    Update ints2bytes.c

--- ints2bytes.c
+++ ints2bytes.c
@@ -1,3 +1,13 @@
+/********************************/
+/* Program Name:  checksum      */
+/* Author: Andreas Hindoyan     */
+/* Date:                        */
+/********************************/
+/* Description:                 */
+/* Validation Checks:           */
+/* Enhancements:                */
+/********************************/
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
@@ -5,39 +15,41 @@
 #define max_int (255)
 #define byte unsigned char
 
-
-int main(int argc, char * argv[], char ** envp)
-{
-
-   int count = 10;
-   int sum=0;
-   int quotient;
-   int remainder;
-   int retval;
-   byte checksum;
-   byte complement;
-   byte header[10];
-
-   retval = read(STDIN_FILENO, &header, 10);
-
-   for(int c = 0; c < count; c++){
-    if(c==5){
-        checksum = header[c];
-        header[c] = 0;
-    }
-    sum += header[c];
-   }
-   quotient = sum / (max_int + 1);
-   remainder = sum % (max_int + 1);
-   sum = quotient + remainder;
-   complement = max_int - sum;
+int main (int argc, char * argv[], char ** envp) {
 
 
-   fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
+  int count = 10;
+  int sum = 0;  
+  int quotient;
+  int remainder;
+  int retval;
+  byte checksum; 
+  byte complement;
+  
+  byte header[10];
+  
+  
+  retval = read(STDIN_FILENO, &header, 10);
+  
+  
+  for (int c = 0; c < count; c++ ) {
+  if ( c == 5 ) {
+  checksum = header[c];
+  header[c] = 0;
+  }
+  sum += header[c];
+  }
+  quotient = (sum / (max_int + 1));             
+  remainder = (sum % (max_int + 1 ));            
+  sum = quotient + remainder;
+  complement = max_int - sum;
+    
+  
+
+  fprintf(stdout, "Stored Checksum: %d, Computed Checksum: %d\n", checksum, complement);
   if (checksum != complement ) {
-    fprintf(stderr, "Error Detected!\n");
+    fprintf(stderr, "Error Detected!\n"); 
     return 1;
   }
   return 0;
 }
-
